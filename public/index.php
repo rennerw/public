0661868e5bd5838be6a8a97c7af624bf042789cc
Wellinghton Silva <rennerwellinghton@gmail.com>
2024-11-25 03:14:20 -0300

    first commit

--- public/index.php
+++ public/index.php
@@ -8,8 +8,25 @@ $app = new Application(dirname(__DIR__));
 
 $app->router->get("/", 'home');
 
-$app->router->get("/contact", 'contact');
+$app->router->get("/cep/{cep}", function($cep){
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://viacep.com.br/ws/$cep[0]/json");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    echo "<pre>";
+    echo(curl_exec($curl));
+    echo "</pre>";
+    curl_close($curl);
+});
+
+$app->router->get("/{cep}", function(){
+    return "Handling cep";
+});
+
 $app->router->post("/contact", function(){
+    return "Handling post";
+});
+
+$app->router->post("/contact/teste", function(){
     return "Handling post";
 });
 
